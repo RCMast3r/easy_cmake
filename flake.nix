5be1d78ff8383590a3cf5ed3680554d8e619489e
Ben Hall <rcmast3r1@gmail.com>
2023-09-29 19:25:59 -0400

    actually working pog

--- flake.nix
+++ flake.nix
@@ -25,25 +25,25 @@
           default = cmake_macros;
         };
 
-  devShells.x86_64-linux.default =
-    pkgs.mkShell rec {
-      # Update the name to something that suites your project.
-      name = "nix-devshell";
-      packages = with pkgs; [
-        # Development Tools
-        cmake
-      ];
+      devShells.x86_64-linux.default =
+        pkgs.mkShell rec {
+          # Update the name to something that suites your project.
+          name = "nix-devshell";
+          packages = with pkgs; [
+            # Development Tools
+            cmake
+          ];
 
-      # Setting up the environment variables you need during
-      # development.
-      shellHook =
-        let
-          icon = "f121";
-        in
-        ''
-          export PS1="$(echo -e '\u${icon}') {\[$(tput sgr0)\]\[\033[38;5;228m\]\w\[$(tput sgr0)\]\[\033[38;5;15m\]} (${name}) \\$ \[$(tput sgr0)\]"
-        '';
+          # Setting up the environment variables you need during
+          # development.
+          shellHook =
+            let
+              icon = "f121";
+            in
+            ''
+              export PS1="$(echo -e '\u${icon}') {\[$(tput sgr0)\]\[\033[38;5;228m\]\w\[$(tput sgr0)\]\[\033[38;5;15m\]} (${name}) \\$ \[$(tput sgr0)\]"
+            '';
+        };
+
     };
-
-};
 }
